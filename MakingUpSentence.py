420dc49edb64eed70390dd06bc57c210f5008d24
Cindy2011-byte <maymyatzaw.app@gmail.com>
2026-05-01 19:42:02 +0630

    Add files via upload
    Type "Peter Piper" in noun1, "peck of pickled peppers" in noun2 and "picked" in verb.

---
+++ MakingUpSentence.py
@@ -0,0 +1,7 @@
+noun1 = input('Enter a noun: ')
+noun2 = input('Enter another noun: ')
+verb = input('Enter a verb: ')
+
+print(f'{noun1} {verb} a {noun2}.')
+print(f'A {noun2} {noun1} {verb}.')
+print(f'If {noun1} {verb} a {noun2}, where are the pickled peppers {noun1} {verb}?')
